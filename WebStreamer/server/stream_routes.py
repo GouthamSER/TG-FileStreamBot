4262b6d501063be19d2bddfe8ec6bbee33060c1d
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-06-21 17:18:20 +0530

    added force fully downlaod

--- WebStreamer/server/stream_routes.py
+++ WebStreamer/server/stream_routes.py
@@ -132,8 +132,7 @@ async def media_streamer(request: web.Request, message_id: int, secure_hash: str
     if not mime_type:
         mime_type = mimetypes.guess_type(file_name)[0] or "application/octet-stream"
 
-    if "video/" in mime_type or "audio/" in mime_type or "/html" in mime_type:
-        disposition = "inline"
+    # forced attachment for all types — no more inline play in browser
 
     return web.Response(
         status=206 if range_header else 200,
